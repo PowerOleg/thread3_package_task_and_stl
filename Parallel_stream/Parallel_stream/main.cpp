43cca397d4705f16e19afbadad13f28d945b2905
PowerOleg <geraskin.oleg@mail.ru>
2024-09-08 16:20:36 +0300

    finally it works

--- Parallel_stream/Parallel_stream/main.cpp
+++ Parallel_stream/Parallel_stream/main.cpp
@@ -21,7 +21,7 @@ std::vector<int> init_filled_vector(const int n)
 {
 	std::vector<int> vector;
 	vector.reserve(n);
-	for (size_t i = 0, j = n - 1; i < n; i++, j--)
+	for (int i = 0, j = n - 1; i < n; i++, j--)
 	{
 		vector.push_back(i);
 	}
@@ -29,38 +29,42 @@ std::vector<int> init_filled_vector(const int n)
 	return vector;
 }
 
-void work()
+/*void work(int number)
 {
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-}
+	std::this_thread::sleep_for(std::chrono::seconds(number));
+	//task();
+}*/
 
-template<typename T>
-void parallel_stream(std::vector<int>::iterator first, std::vector<int>::iterator last, T&& /*std::function<void()> const&*/  /*std::unique_ptr<void()>*/ lambda)
+//template<typename T>
+void parallel_stream(std::vector<int>::iterator first, std::vector<int>::iterator last, /*T&&*/ std::function<int(int)> const& function)
 {
+	std::transform(first, last, first, function);
+	std::for_each(first, last, [](int n) {std::cout << n << " "; });
+	std::cout << std::endl;
+
+
 	unsigned int cores_quantity = std::thread::hardware_concurrency();
-	//create package_task and run
-	for (size_t i = 0; i < cores_quantity - 2; i++)
-	{
+	//std::packaged_task<int()> task(std::bind(parallel_stream, vector.begin(), vector.end(), std::move(function)));
+	//std::future<int> future = task.get_future();
+	//std::thread t1(std::move(task));
+	//future.get();
+	//t1.join();
 
-	}
 
-	std::for_each(first, last, lambda);
-
+	//std::execution::seq дл€ последовательного выполнени€ алгоритма;
+	//используйте std::execution::par дл€ паралельного выполнени€
 }
 
+//auto p_lambda = std::make_unique<void()>(lambda);
 int main(int argc, char** argv)
 {
 	const int size = 10;
 	std::vector<int> vector = init_filled_vector(size);
+	auto lambda = [](int n) { return n * n;	};
+	std::function<int(int)> function(lambda);
+	parallel_stream(vector.begin(), vector.end(), std::move(function));
 
-	auto lambda = [](int n) { return ++n; };
-	//std::function<void()> function  = lambda;
-	//std::shared_ptr<std::unique_ptr<int>> pplambda = std::make_shared<std::unique_ptr<int>>(std::move(lambda));
-	auto plambda = std::make_unique<void()>(lambda);
-	
-
-	parallel_stream(vector.begin(), vector.end(), std::move(lambda));
-	
+	print(vector, 10);
 
 	return 0;
 }
